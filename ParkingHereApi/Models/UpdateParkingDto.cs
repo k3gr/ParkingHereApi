ad9a082e850fbdc59fe85e798428ec9bf82423ee
k3gr <k3grzesik@gmail.com>
2023-05-07 12:02:53 +0200

    Add Parking controller.

---
+++ ParkingHereApi/Models/UpdateParkingDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ParkingHereApi.Models
+{
+    public class UpdateParkingDto
+    {
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+        public string ContactEmail { get; set; }
+        public string ContactNumber { get; set; }
+    }
+}
--- ParkingHereApi/Services/ParkingService.cs
+++ ParkingHereApi/Services/ParkingService.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
 using ParkingHereApi.Entities;
 using ParkingHereApi.Models;
@@ -15,6 +16,7 @@ namespace ParkingHereApi.Services
             _dbContext = dbContext;
             _mapper = mapper;
         }
+
         public IEnumerable<ParkingDto> GetAll() 
         {
             var parkings = _dbContext
@@ -27,13 +29,52 @@ namespace ParkingHereApi.Services
 
             return parkingsDtos;
         }
+
         public int Create(CreateParkingDto dto)
         {
-            var restaurant = _mapper.Map<Parking>(dto);
-            _dbContext.Parkings.Add(restaurant);
+            var parking = _mapper.Map<Parking>(dto);
+            _dbContext.Parkings.Add(parking);
             _dbContext.SaveChanges();
 
-            return restaurant.Id;
+            return parking.Id;
+        }
+
+        public void Update(int id, UpdateParkingDto dto)
+        {
+            var parking = _dbContext
+                .Parkings
+                .FirstOrDefault(p => p.Id == id);
+
+            parking.Name = dto.Name;
+            parking.Description = dto.Description;
+            parking.Type = dto.Type;
+            parking.ContactNumber = dto.ContactNumber;
+            parking.ContactEmail = dto.ContactEmail;
+
+            _dbContext.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var parking = _dbContext
+                .Parkings
+                .FirstOrDefault(p => p.Id == id);
+
+            _dbContext.Parkings.Remove(parking);
+            _dbContext.SaveChanges();
+        }
+
+        public ParkingDto GetById(int id)
+        {
+            var parking = _dbContext
+                .Parkings
+                .Include(p => p.Address)
+                .Include(p => p.Spots)
+                .FirstOrDefault(p => p.Id == id);
+
+            var result = _mapper.Map<ParkingDto>(parking);
+
+            return result;
         }
     }
 }
